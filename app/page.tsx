116840dc26087ab5385f5940fd751f594a6385e0
June <junimiso04@naver.com>
2023-06-23 02:27:58 +0900

    feat(ui): add community component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
-import NextLink from "next/link";
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import {Community} from "@/components/marketing/community";
 
 export default function Home() {
 	return (
 	    <main className="container mx-auto max-w-7xl px-6 pt-16 flex-grow">
 		    <section className="flex flex-col items-center justify-center">
-				
+			    <Community />
 		    </section>
 	    </main>
 	);
--- components/primitives.ts
+++ components/primitives.ts
@@ -1,5 +1,9 @@
 import { tv } from "tailwind-variants";
 
+export const titleWrapper = tv({
+	base: "flex flex-col gap-2 items-start justify-center w-full",
+});
+
 export const title = tv({
 	base: "tracking-tight inline font-semibold",
 	variants: {
@@ -40,8 +44,28 @@ export const title = tv({
 	],
 });
 
+export const sectionWrapper = tv({
+	base: "relative z-10 flex flex-col gap-2 w-full",
+	variants: {
+	  isBlurred: {
+		true: [
+		  "bg-transparent",
+		  "dark:bg-transparent",
+		  "before:bg-background/10",
+		  "before:content-['']",
+		  "before:block",
+		  "before:z-[-1]",
+		  "before:absolute",
+		  "before:inset-0",
+		  "before:backdrop-blur-md",
+		  "before:backdrop-saturate-200",
+		],
+	  },
+	},
+});
+
 export const subtitle = tv({
-	base: "w-full md:w-1/2 my-2 text-lg lg:text-xl text-default-600 block max-w-full",
+	base: "w-full md:w-1/2 my-2 text-lg lg:text-xl text-default-500 block max-w-full",
 	variants: {
 		fullWidth: {
 			true: "!w-full",
