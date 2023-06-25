2e15d82cab78f7160777e315252ad5c4fd720b2d
June <junimiso04@naver.com>
2023-06-25 17:22:45 +0900

    feat(ui): fix viewport error at sub pages

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -2,8 +2,10 @@ import { title } from "@/components/primitives";
 
 export default function DocsPage() {
 	return (
-		<div>
-			<h1 className={title()}>Docs</h1>
-		</div>
+		<section className="container mx-auto max-w-7xl px-6 pt-16 flex-grow">
+            <section className="flex flex-col items-center justify-center">
+                <h1 className={title()}>Docs</h1>
+            </section>
+        </section>
 	);
 }
--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -2,8 +2,10 @@ import { title } from "@/components/primitives";
 
 export default function FeedBackPage() {
 	return (
-		<div>
-			<h1 className={title()}>FeedBack</h1>
-		</div>
+		<section className="container mx-auto max-w-7xl px-6 pt-16 flex-grow">
+            <section className="flex flex-col items-center justify-center">
+                <h1 className={title()}>Feedback</h1>
+            </section>
+        </section>
 	);
 }
