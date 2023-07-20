db23b8943786c689ca2b2c9cca5518ef6cd31780
June <junimiso04@naver.com>
2023-07-20 18:44:36 +0900

    feat: initialize repo

--- components/icons/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./bold";
-export * from "./linear";
-export * from "./logo";
-export * from "./social";
-export * from "./sun-and-moon";
--- config/fonts.ts
+++
@@ -1,9 +0,0 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google"
-
-export const fontSans = FontSans({
-  subsets: ["latin"],
-})
-
-export const fontMono = FontMono({
-  subsets: ["latin"],
-})
